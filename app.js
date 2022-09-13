97b02c1c0037bc420b0204cbc915f1e5facac17a
AlmostIvanSidorov <vanosidorov@gmail.com>
2022-09-13 17:25:15 +0300

    scale

--- app.js
+++ app.js
@@ -43,3 +43,13 @@ function switchImage(node) {
         }
     }
 }
+
+var siteWidth = 1280
+var scale = screen.width / siteWidth
+
+document
+    .querySelector('meta[name="viewport"]')
+    .setAttribute(
+        'content',
+        'width=' + siteWidth + ', initial-scale=' + scale + ''
+    )
